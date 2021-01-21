9884f1eef663eec5e17f3232b9605805fbc4a59d
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-01-21 15:39:24 +0100

    Removed unusing napespaces

--- src/Presentation/Hubs/GameCreatorHub.cs
+++ src/Presentation/Hubs/GameCreatorHub.cs
@@ -1,8 +1,6 @@
 ﻿using Domain.Lists;
 using Domain.Models;
 using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
