a542a94c485cf4b7ad6758d82c8e67aaab2148d0
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-05-04 09:08:29 +0200

    Refactor DependencyInjections

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -8,8 +8,7 @@ namespace Infrastructure
     public static class DependencyInjection
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
-        {
-            
+        {         
             services.AddMvc().AddNToastNotifyToastr(new ToastrOptions()
             {
                 ProgressBar = false,
